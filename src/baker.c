915c0269033732ed07e9c6268dace764bfc37ac9
toweroftop <37008641+toweroftop@users.noreply.github.com>
2021-06-27 15:45:27 +0800

    Update baker.c

--- src/baker.c
+++ src/baker.c
@@ -12,19 +12,20 @@ Baker
 #include<pthread.h>
 #include<string.h>
 
-void *p1FUN(void *data);
-void *p2FUN(void *data);
+void *baker(void *data);
+void *consumer(void *data);
 
 pthread_cond_t pCond;
 pthread_mutex_t pMutex;
-
+int n = 0;
 
 int main(int argn, char *argc[])
 {
-	pthread_t p1, p2;
+	pthread_t *p1, p2; //p1 baker p2 consumer
 	pthread_attr pAttr;
-	int backv;
-
+	int backv, i;
+	printf("Input baker number:\n");
+	scanf("%d", &n);
 	//Init pthread Attribute
 	if ((backv = pthread_attr_init(&pAttr)) != 0)
 	{
@@ -47,12 +48,19 @@ int main(int argn, char *argc[])
 	}
 
 	//POSIX thread Init
-	if ((pthread_create(&p1, &pAttr, p1FUN, NULL)) != 0)
+	p1 = malloc(sizeof(pthread_t) * n);
+	for (i = 0; i < n; i++)
 	{
-		printf("thread 1 create failure\n");
-		return -2;
+		backv = backv | pthread_create(&p1[i], &pAttr, baker, &i);
+		if (backv != 0)
+		{
+			printf("backer thread create failure\n");
+			return -2;
+		}
 	}
-	if ((pthread_create(&p2, &pAttr, p2FUN, NULL)) != 0)
+
+
+	if ((pthread_create(&p2, &pAttr, comsumer, NULL)) != 0)
 	{
 		printf("thread 2 create failure\n");
 		return -2;
@@ -63,12 +71,14 @@ int main(int argn, char *argc[])
 	return 0;
 }
 
-void *p1FUN(void *data)
+void *baker(void *data)
 {
 	int num = 0;
+	int sig = (int)(*data);
+	printf("baker %d working.\n", sig);
 }
 
-void *p2FUN(void *data)
+void *consumer(void *data)
 {
 	int num = 0;
 }
